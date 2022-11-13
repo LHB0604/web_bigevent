61df60c578aac54586547a78596e685287202b05
LHB <565386089@qq.com>
2022-11-13 19:39:31 +0800

    完成登录和注册功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,29 +1,10 @@
+//注意:每次调用 $.get 或 $.post 或 $.ajax()时候
+//会先调用这个函数 ajaxPrefilter
+//在这函数中能拿到我们给ajax提供的配置对象
 
-// 后续，如果哪个页面需要发送ajax请求，则必须加载baseAPI.js
-$.ajaxPrefilter(function (option) {
-    // option 就是每次ajax请求时的配置对象
+$.ajaxPrefilter(function(options){
+    //再发起真正的ajax之前，统一拼接的根路径
+    options.url = 'http://www.liulongbin.top:3007/' + options.url
+    // console.log(options.url);
 
-    // console.log(option);
-    // 配置url，统一加上根路径
-    option.url = 'http://www.liulongbin.top:3007' + option.url;
-
-    // z请求的url，如果是以  /my/ 开头的，则需要进行下面两项配置
-    if (option.url.indexOf('/my/') !== -1) {
-        // 配置complete
-        // complete，当ajax请求完成之后触发（无论ajax请求成功还是失败都会触发）
-        option.complete = function (xhr) {
-            // console.log('xhr=', xhr);
-            if (xhr.responseJSON.status === 1 && xhr.responseJSON.message === '身份认证失败！') {
-                // 清除假token
-                localStorage.removeItem('token');
-                // 说明用户没有登录
-                location.href = '/login.html'
-            }
-        }
-        // 配置headers
-        option.headers = {
-            Authorization: localStorage.getItem('token')
-        }
-    }
-    
-});
+})
